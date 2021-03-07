5f65d848bcf5d382ff3edba3c3f3b4117a66fd1a
Charles Lai <charlesangeloplai@gmail.com>
2021-03-07 15:13:06 -0500

    commit tree 6db6c93b0181de210d360c6a5d8c00831dfe59fa

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, func) {
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(func(array[i]));
+  }
+
+  return newArray;
+}
+
+function reduce(array, func, start = 0) {
+  let newArray = array[start];
+
+  for (let i = start; i < array.length; i++) {
+    newArray = func(array[i]);
+  }
+
+  return newArray;
+}
